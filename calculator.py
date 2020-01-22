b6adb20da54a0c6168be8b0cf536535545b53c4f
Mario Slongo <mslongo@live.ca>
2020-01-21 21:25:39 -0800

    Added save buttons and fixed minor bugs

--- calculator.py
+++ calculator.py
@@ -18,22 +18,45 @@ class Mywindow(QtWidgets.QMainWindow):
 		self.curr = 0
 		self.initialize_buttons()
 		self.initial = True
+		self.operation_done = False
 
 	def initialize_buttons(self):
 		for button in self.ui.numbers.buttons():
 			button.clicked.connect(self.set_number(button.text()))
 		for operator in self.ui.operators.buttons():
 			operator.clicked.connect(self.op_clicked(operator.text()))
+		for saved in self.ui.saved.buttons():
+			saved.clicked.connect(self.save_clicked(saved.text()))
 		self.ui.button_equals.clicked.connect(self.equals_clicked)
 		self.ui.button_sin.clicked.connect(self.sin_clicked)
 
+	def save_clicked(self, value):
+		def save_clicked():
+			try:
+				number = float(value)
+				self.set_number(number)
+			except ValueError:
+				set_number = float(self.ui.main_line.text()) if self.curr != 0 else 0
+				if value == "A":
+					self.ui.button_A.setText("%.3f" % set_number)
+				elif value == "B":
+					self.ui.button_B.setText("%.3f" % set_number)
+				elif value == "C":
+					self.ui.button_C.setText("%.3f" % set_number)
+				elif value == "D":
+					self.ui.button_D.setText("%.3f" % set_number)
+		return save_clicked
+
 	def op_clicked(self, op):
 		def operation():
+			if self.operation_done:
+				self.equals_clicked()
 			self.currentop = op
 			# self.do_operation(op)
 			# self.first_num = self.curr
 			self.first = False
 			# self.set_line(self.curr)
+			self.operation_done = True
 		return operation
 
 	def do_operation(self, op):
@@ -54,12 +77,17 @@ class Mywindow(QtWidgets.QMainWindow):
 		self.curr = math.sin(self.curr)
 		self.set_line(self.curr)
 
+	def cos_clicked(self):
+		self.curr = math.cos(self.curr)
+		self.set_line(self.curr)
+
 	def equals_clicked(self):
 		self.do_operation(self.currentop)
 		self.first_num = self.curr
 		self.second_num = 0
 		self.first = True
 		self.set_line(self.curr)
+		self.operation_done = False
 
 	def set_number(self, number):
 		def print_number():
@@ -85,6 +113,7 @@ class Mywindow(QtWidgets.QMainWindow):
 		self.ui.main_line.setText(str(number))
 
 	def clear(self):
+		self.operation_done = False
 		self.first_num = 0
 		self.second_num = 0
 		self.curr = 0
