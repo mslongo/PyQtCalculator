f19cba008925830555bab1874e219c70fccac636
Mario Slongo <mslongo@live.ca>
2020-01-22 20:43:20 -0800

    Fixed saved numbers and added basic decimal functionality

--- calculator.py
+++ calculator.py
@@ -19,6 +19,8 @@ class Mywindow(QtWidgets.QMainWindow):
 		self.initialize_buttons()
 		self.initial = True
 		self.operation_done = False
+		self.last_equal_clicked = False
+		self.dec_mode = False
 
 	def initialize_buttons(self):
 		for button in self.ui.numbers.buttons():
@@ -26,17 +28,38 @@ class Mywindow(QtWidgets.QMainWindow):
 		for operator in self.ui.operators.buttons():
 			operator.clicked.connect(self.op_clicked(operator.text()))
 		for saved in self.ui.saved.buttons():
-			saved.clicked.connect(self.save_clicked(saved.text()))
+			saved.clicked.connect(self.save_clicked(saved))
 		self.ui.button_equals.clicked.connect(self.equals_clicked)
 		self.ui.button_sin.clicked.connect(self.sin_clicked)
+		self.ui.button_cos.clicked.connect(self.cos_clicked)
+		self.ui.button_dec.clicked.connect(self.dec_check)
 
-	def save_clicked(self, value):
+	def dec_check(self):
+		if not self.dec_mode:
+			self.dec_mode = True
+			if self.first:
+				self.first_num = (str(self.first_num) + '.')
+				set_number = self.first_num
+			else:
+				self.second_num = (str(self.second_num) + '.')
+				set_number = self.second_num
+			self.set_line(set_number)
+
+
+	def save_clicked(self, button):
 		def save_clicked():
+			value = button.text()
 			try:
 				number = float(value)
-				self.set_number(number)
+				if self.first:
+					self.first_num = number
+					set_number = self.first_num
+				else:
+					self.second_num = number
+					set_number = self.second_num
+				self.set_line(set_number)
 			except ValueError:
-				set_number = float(self.ui.main_line.text()) if self.curr != 0 else 0
+				set_number = float(self.ui.main_line.text()) if (self.curr != 0 and float(self.ui.main_line.text()) != 0) else 0
 				if value == "A":
 					self.ui.button_A.setText("%.3f" % set_number)
 				elif value == "B":
@@ -49,8 +72,10 @@ class Mywindow(QtWidgets.QMainWindow):
 
 	def op_clicked(self, op):
 		def operation():
+			self.dec_mode = False
+			self.last_equal_clicked = False
 			if self.operation_done:
-				self.equals_clicked()
+				self.op_equals()
 			self.currentop = op
 			# self.do_operation(op)
 			# self.first_num = self.curr
@@ -61,6 +86,8 @@ class Mywindow(QtWidgets.QMainWindow):
 
 	def do_operation(self, op):
 		self.first = False
+		self.first_num = float(self.first_num)
+		self.second_num = float(self.second_num)
 		if op == "+":
 			self.curr = self.first_num + self.second_num
 		elif op == "-":
@@ -81,7 +108,7 @@ class Mywindow(QtWidgets.QMainWindow):
 		self.curr = math.cos(self.curr)
 		self.set_line(self.curr)
 
-	def equals_clicked(self):
+	def op_equals(self):
 		self.do_operation(self.currentop)
 		self.first_num = self.curr
 		self.second_num = 0
@@ -89,16 +116,35 @@ class Mywindow(QtWidgets.QMainWindow):
 		self.set_line(self.curr)
 		self.operation_done = False
 
+	def equals_clicked(self):
+		self.dec_mode = False
+		self.do_operation(self.currentop)
+		self.first_num = self.curr
+		self.second_num = 0
+		self.first = True
+		self.set_line(self.curr)
+		self.operation_done = False
+		self.last_equal_clicked = True
+
 	def set_number(self, number):
 		def print_number():
+			if self.last_equal_clicked:
+				self.last_equal_clicked = False
+				self.first_num = 0
 			try:
 				if self.first:
-					check = str(number)
-					self.first_num = int(str(self.first_num) + str(check))
+					check = int(number)
+					if not self.dec_mode:
+						self.first_num = (int(self.first_num) + int(check))
+					else:
+						self.first_num = (str(self.first_num) + str(check))
 					set_number = self.first_num
 				else:
-					check = str(number)
-					self.second_num = int(str(self.second_num) + str(check))
+					check = int(number)
+					if not self.dec_mode:
+						self.second_num = (int(self.second_num) + int(check))
+					else:
+						self.second_num = (str(self.second_num) + str(check))
 					set_number = self.second_num
 				self.set_line(set_number)
 			except ValueError:
